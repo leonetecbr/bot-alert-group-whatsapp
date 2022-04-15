7c4f9184772c38d962c2b84dcb44894f346de1f0
Leone Oliveira <falecom@leone.tec.br>
2022-04-15 12:04:48 -0300

    Retira o aviso por mensagem

--- components/AlertUsers.js
+++ components/AlertUsers.js
@@ -1,7 +1,6 @@
 const ALERTS = require('../resources/alerts.json')
 const database = require('../databases/db')
 const User = require('../models/User')
-const admin = require('../resources/admin.json')
 
 async function AlertUsers(message, alertId, client){
     await database.sync()
@@ -13,18 +12,13 @@ async function AlertUsers(message, alertId, client){
     if (users !== null){
         let text = 'Oi, você tem um novo alerta para *' + ALERTS[alertId] + '*\n\n*' + message.sender.pushname + '* mandou a ' +
             'seguinte mensagem no grupo *' + message.chat.name + '*: \n\n'
-        let notSend = []
         text += message.text
         for (let i = 0; typeof users[i] !== "undefined"; i++){
             if (users[i].id !== message.author){
                 let sended = await client.sendText(users[i].id, text)
-                if (typeof sended !== 'string') notSend.push(users[i].id)
-                else if (!sended.startsWith('true')) notSend.push(users[i].id)
+                if (typeof sended !== 'string') console.log(users[i])
+                else if (!sended.startsWith('true')) console.log(users[i])
             }
-        }
-
-        if (notSend.length !== 0){
-            await client.sendText(admin[0], notSend.join())
         }
     }
 }
