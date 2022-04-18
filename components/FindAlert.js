9f6928db52a5e961c54bf647ad3fe03ab92efc4a
Leone Oliveira <falecom@leone.tec.br>
2022-04-18 17:21:28 -0300

    Melhora o envio de alertas

--- components/FindAlert.js
+++ components/FindAlert.js
@@ -5,7 +5,7 @@ async function FindAlert(message, client) {
     let alerted = false
     for (let i = 1; typeof ALERTS[i] !== 'undefined'; i++) {
         if (message.text.toLowerCase().search(ALERTS[i]) !== -1) {
-            if (message.text.length === ALERTS[i].length && message.quotedMsgObj !== null){
+            if (message.text.length === ALERTS[i].length && message.quotedMsgObj !== null && message.quotedMsgObj.text !== ''){
                 await alertUsers(message.quotedMsgObj, i, client)
             } else await alertUsers(message, i, client)
             alerted = true
