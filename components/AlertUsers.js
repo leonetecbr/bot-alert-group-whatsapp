72ef8e3889b6fb16251c1b355fdcffbefdf62154
Leone Oliveira <falecom@leone.tec.br>
2022-06-15 18:15:51 -0300

    Resolve os problemas no arquivo de administradores

--- components/AlertUsers.js
+++ components/AlertUsers.js
@@ -7,21 +7,21 @@ async function AlertUsers(message, alertId, client){
     let where = {}
     where['a'+alertId] = true
 
-    let users = await User.findAll({where: where})
+    let users = await User.findAll({where: where, raw: true})
 
     // Se existirem usuários com o alerta ativado
-    if (users !== null){
+    if (users.length !== 0){
         let text = 'Oi, você tem um novo alerta para *' + ALERTS[alertId] + '*\n\n*' + message.sender.pushname + '* mandou a ' +
             'seguinte mensagem no grupo *' + message.chat.name + '*: \n\n'
         text += message.text
 
-        for (let i = 0; typeof users[i] !== "undefined"; i++){
+        for (let i = 0; i < users.length; i++){
             // Se não for o usuário que lançou o alerta
             if (users[i].id !== message.author){
                 // Envie o alerta
                 let sended = await client.sendText(users[i].id, text)
-                // Se não for enviado, registre no console
-                if (typeof sended !== 'string') console.log(users[i])
+                // Registra no console
+                console.log(sended)
             }
         }
     }
