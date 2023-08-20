be1d23742d08349349ed00cebd568c1584ec7de5
Leone Oliveira <falecom@leone.tec.br>
2023-08-20 12:10:09 -0300

    refactor: Volta a deletar alertas quando a mensagem original é apagada

--- components/ProcessDeletion.js
+++ components/ProcessDeletion.js
@@ -3,7 +3,7 @@ const {Alerted} = require('../models')
 module.exports = async (client, messageDeleted, revokedMessage) => {
     const message = revokedMessage ?? messageDeleted
 
-    if (message.from === undefined) return false
+    if (message.id === undefined) return false
 
     const alerteds = await Alerted.findAll({
         where: {
@@ -14,8 +14,25 @@ module.exports = async (client, messageDeleted, revokedMessage) => {
     })
 
     for (const alerted of alerteds) {
-        // TODO: Delete message alerted.messageId
-        console.log('|***** Mensagem deletada:  ', alerted.messageId, '*****|')
+        client.getChatById(alerted.messageId.split('_')[1])
+            .then(chat => {
+                let limit = 20
+
+                if (chat.isGroup) limit *= chat.participants.length
+
+                chat.fetchMessages({limit})
+                    .then(messages => {
+                        messages.map((message) => {
+                            if (message.id._serialized === alerted.messageId) {
+                                message.delete(true)
+                                    .then(() => console.log('|***** Mensagem deletada:  ', alerted.messageId, '*****|'))
+                                    .catch(e => console.log(e))
+                            }
+                        })
+                    })
+                    .catch(e => console.log(e))
+            })
+            .catch(e => console.log(e))
         await alerted.destroy()
     }
 }
