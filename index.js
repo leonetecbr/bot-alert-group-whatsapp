b6eb78177407561fb6846e04fd7d619959cb0034
Leone Oliveira <falecom@leone.tec.br>
2023-08-31 14:28:47 -0300

    refactor: Pequenas melhorias

--- index.js
+++ index.js
@@ -12,59 +12,66 @@ const processDeletion = require('./components/ProcessDeletion')
 const processAddGroup = require('./components/ProcessAddGroup')
 
 async function start() {
-    console.log('Iniciando ...')
+    try {
+        console.log('Iniciando ...')
 
-    const client = new Client({
-        authStrategy: new LocalAuth(),
-    })
-    let lastMessage = null
+        const client = new Client({
+            authStrategy: new LocalAuth(),
+        })
+        let lastMessage = null
 
-    // Pronto para mostrar o QR Code
-    client.on('qr', qr => {
-        console.log('Escanei o QR Code:')
-        qrcode.generate(qr, {small: true})
-    })
+        // Pronto para mostrar o QR Code
+        client.on('qr', qr => {
+            console.log('Escanei o QR Code:')
+            qrcode.generate(qr, {small: true})
+        })
 
-    // Autenticado
-    client.on('authenticated', session => console.log('Autenticado com sucesso!'))
+        // Autenticado
+        client.on('authenticated', session => console.log('Autenticado com sucesso!'))
 
-    // WhatsApp conectado
-    client.on('ready', () => {
-        console.log('Iniciado com sucesso!')
-        // Reinicia o robô todos os dias para evitar bugs
-        setTimeout(() => {
-             client.destroy()
-                .then(start)
-                .catch(e => console.log(e))
-        }, 24 * 60 * 60 * 1000)
-    })
+        // WhatsApp conectado
+        client.on('ready', () => {
+            console.log('Iniciado com sucesso!')
+            // Reinicia o robô todos os dias para evitar bugs
+            setTimeout(() => {
+                client.destroy()
+                    .then(() => {
+                        start().then((e) => e ? console.log(e) : '')
+                        throw 'Encerrando função anterior e iniciando nova';
+                    })
+                    .catch(e => console.log(e))
+            }, 24 * 60 * 60 * 1000)
+        })
 
-    // Mensagem recebida
-    client.on('message', async message => {
-        message.lastMessage = lastMessage
-        lastMessage = await processMessage(client, message)
-    })
+        // Mensagem recebida
+        client.on('message', async message => {
+            message.lastMessage = lastMessage
+            lastMessage = await processMessage(client, message)
+        })
 
-    // Mensagem deletada para todos
-    client.on('message_revoke_everyone', async (message, revoked_msg) => await processDeletion(client, message, revoked_msg))
+        // Mensagem deletada para todos
+        client.on('message_revoke_everyone', async (message, revoked_msg) => await processDeletion(client, message, revoked_msg))
 
-    // Chamada recebida
-    client.on('incoming_call', async call => await processCall(client, call))
+        // Chamada recebida
+        client.on('incoming_call', async call => await processCall(client, call))
 
-    // Estado mudou
-    client.on('change_state', state => {
-        console.log('Mudou de estado:', state)
+        // Estado mudou
+        client.on('change_state', state => {
+            console.log('Mudou de estado:', state)
 
-        if (state === 'CONFLICT' || state === 'UNLAUNCHED') client.resetState()
-    })
+            if (state === 'CONFLICT' || state === 'UNLAUNCHED') client.resetState()
+        })
 
-    // Foi adicionado em um grupo
-    client.on('group_join', notification => processAddGroup(client, notification))
+        // Foi adicionado em um grupo
+        client.on('group_join', notification => processAddGroup(client, notification))
 
-    // Desconectado
-    client.on('disconnected', e => console.log('Desconectado do Whatsapp!!!', e))
+        // Desconectado
+        client.on('disconnected', e => console.log('Desconectado do Whatsapp!!!', e))
 
-    await client.initialize()
+        await client.initialize()
+    } catch (e) {
+        return e;
+    }
 }
 
-start()
+start().then((e) => e ? console.log(e) : '')
