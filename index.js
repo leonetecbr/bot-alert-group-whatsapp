c120869be904690404eeea1bd6d1f8d3f291e7e9
Leone Oliveira <falecom@leone.tec.br>
2024-05-15 13:46:28 -0300

    build: Atualiza versões

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ async function start() {
             authStrategy: new LocalAuth(),
             webVersionCache: {
                 type: 'remote',
-                remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
+                remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2413.51-beta.html',
             }
         })
         let lastMessage = []
